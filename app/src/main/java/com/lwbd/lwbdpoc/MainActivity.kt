6a24218de77505d267224936e521c67534dbf745
ruhul38 <91838789+ruhul38@users.noreply.github.com>
2024-10-02 04:42:08 +0600

    chore(setup): initial commit
    Added the default Android project setup using the Basic Activity template.

---
+++ app/src/main/java/com/lwbd/lwbdpoc/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.lwbd.lwbdpoc
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.lwbd.lwbdpoc.ui.theme.LWBDPOCTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LWBDPOCTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    LWBDPOCTheme {
+        Greeting("Android")
+    }
+}
