6688e666e725cae514087992ae679e17fdb48b5b
ruhul38 <91838789+ruhul38@users.noreply.github.com>
2024-10-05 20:42:34 +0600

    feat: add JankStatsModule for performance monitoring
    - Implement JankStatsModule to provide dependencies for tracking jank frames in the app
    - Include OnFrameListener to log jank frames
    - Provide Window instance for JankStats

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,11 @@ android {
 dependencies {
 
     implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.core.splashscreen)
+    implementation(libs.androidx.hilt.navigation.compose)
+    implementation(libs.androidx.lifecycle.runtimeCompose)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.tracing.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
